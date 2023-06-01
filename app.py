47d7747d3fe0a66f7b8311d2434115413f59755d
hoga5901 <135210077+hoga5901@users.noreply.github.com>
2023-06-01 17:33:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,7 +66,7 @@ def callback():
         if not isinstance(event.message, TextMessage):
             continue
             
-        reslut = pn.read( event.message.text )
+        rest = pn.read( event.message.text )
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text=result)
